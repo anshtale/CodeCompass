db114a4f03fa9e0246dcd6a584b31b3914c58491
Ansh Tale <anshtale2004@gmail.com>
2025-03-19 14:54:56 +0530

    added clerk authentication, added route for updating db for new user

---
+++ src/app/(protected)/layout.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type props = {
+    children: React.ReactNode
+}
+
+function SideBarLayout([children] : props) {
+  return (
+    
+  )
+}
+
+export default SideBarLayout
