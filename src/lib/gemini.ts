6f8c655f6a4ef785ca7118678589d0e07bdd1061
Ansh Tale <anshtale2004@gmail.com>
2025-03-21 02:12:30 +0530

    added function for summarising the code and for generating embeddings for the summary

--- src/lib/gemini.ts
+++ src/lib/gemini.ts
@@ -1,4 +1,5 @@
 import { GoogleGenerativeAI } from "@google/generative-ai"
+import {Document} from '@langchain/core/documents'
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!)
 
@@ -49,6 +50,37 @@ export const AIsummariseCommit = async(diff:string)=>{
     ])
 
     return response.response.text();
+}
+
+export async function summariseCode(doc : Document){
+    const code = doc.pageContent.slice(0,10000);
+
+    const response = await model.generateContent([
+        `You are an intelligent senior software engineer who specialises in onboarding junior engineers onto projects`,`
+        You are onboarding a junior software engineer and explaining to them the purpose of the ${doc.metadata.source} file
+        
+        Here is the code:
+        -----------
+
+        ${code}
+
+        -----------
+                Give a summary no more than 100 words of the code above` 
+    ]);
+    return response.response.text();
+
+}
+
+
+export async function generateEmbeddings(summary:string){
+    const model = genAI.getGenerativeModel({
+        model:'text-embedding-004'
+    })
+
+    const result = await model.embedContent(summary)
+    const embedding = result.embedding
+
+    return embedding.values
 }
 
 
